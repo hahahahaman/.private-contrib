0252eaf8be06441eeb92acba494bfe3d60b2f0e8
hahahahaman <hahahadude@gmail.com>
2015-09-09 13:43:13 -0400

    slime layer, and (misc) stuff layer

---
+++ slime/packages.el
@@ -0,0 +1,114 @@
+;;; packages.el --- slime Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq slime-packages
+  '(slime
+    slime-company))
+
+(defun slime/init-slime ()
+  (use-package slime
+    :commands slime-mode
+    :init
+    (progn
+      (setq slime-contribs '(slime-fancy
+                             slime-indentation
+                             slime-sbcl-exts
+                             slime-scratch
+                             ;; slime-mdot-fu
+                             ;; slime-trace-dialog
+                             )
+            inferior-lisp-program "sbcl")
+      ;; enable fuzzy matching in code buffer and SLIME REPL
+
+      (setq slime-complete-symbol*-fancy t)
+      (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+      ;; enable smartparen in code buffer and SLIME REPL
+      ;; (add-hook 'slime-repl-mode-hook #'smartparens-strict-mode)
+      (defun slime/disable-smartparens ()
+        (smartparens-strict-mode -1)
+        (turn-off-smartparens-mode))
+      (add-hook 'slime-repl-mode-hook #'slime/disable-smartparens)
+      (spacemacs/add-to-hooks 'slime-mode '(lisp-mode-hook)))
+    :config
+    (progn
+      (dolist (m `(,slime-mode-map ,slime-repl-mode-map))
+        (define-key m [(tab)] 'slime-fuzzy-complete-symbol))
+      (dolist (m '(lisp-mode
+                   slime-repl-mode))
+        (evil-leader/set-key-for-mode m
+          "mcc" 'slime-compile-file
+          "mcC" 'slime-compile-and-load-file
+          "mcf" 'slime-compile-defun
+          "mcr" 'slime-compile-region
+
+          "mdb" 'slime-interrupt
+          "mdi" 'slime-inspect-definition
+          "mdI" 'slime-inspect
+
+          "meb" 'slime-eval-buffer
+          "mef" 'slime-eval-defun
+          "mee" 'slime-eval-last-sexp
+          "mer" 'slime-eval-region
+
+          "mgb" 'slime-pop-find-definition-stack
+          "mgg" 'slime-edit-definition
+          "mgG" 'slime-edit-definition-other-window
+          "mgn" 'slime-next-note
+          "mgN" 'slime-previous-note
+          "mgp" 'slime-previous-note
+
+          "mha" 'slime-apropos
+          "mhd" 'slime-disassemble-symbol
+          "mhD" 'slime-disassemble-definition
+          "mhf" 'slime-describe-function
+          "mhh" 'slime-hyperspec-lookup
+          "mhs" 'slime-describe-symbol
+
+          "mma" 'slime-macroexpand-all
+          "mme" 'slime-macroexpand-1
+
+          "mpf" 'slime-toggle-profile-fdefinition
+          "mpF" 'slime-profiled-functions
+          "mpp" 'slime-profile-package
+          "mpr" 'slime-profile-report
+          "mpR" 'slime-profile-reset
+          "mps" 'slime-profile-by-substring
+          "mpu" 'slime-unprofile-all
+
+          "msc" 'slime-cd
+          "mse" 'slime-eval-last-expression-in-repl
+          "msi" 'slime
+          "msp" 'slime-pwd
+          "msq" 'slime-quit-lisp
+          "msr" 'slime-restart-inferior-lisp
+
+          "mtf" 'slime-toggle-fancy-trace
+          "mtt" 'slime-toggle-trace-fdefinition
+          "mtu" 'slime-untrace-all
+
+          "mwb" 'slime-who-binds
+          "mwc" 'slime-who-calls
+          "mwm" 'slime-who-macroexpands
+          "mwr" 'slime-who-references
+          "mws" 'slime-who-sets
+          "mwS" 'slime-who-specializes)))))
+
+(defun stuff/init-slime-company ()
+  (use-package slime-company
+    :config
+    (slime-setup '(slime-company))
+    (progn
+      (define-key company-active-map (kbd "\C-n") 'company-select-next)
+      (define-key company-active-map (kbd "\C-p") 'company-select-previous)
+      (define-key company-active-map (kbd "\C-d") 'company-show-doc-buffer)
+      (define-key company-active-map (kbd "<tab>") 'company-complete))))
+
---
+++ stuff/packages.el
@@ -0,0 +1,67 @@
+;;; packages.el --- stuff Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;; List of all packages to install and/or initialize. Built-in packages
+;; which require an initialization must be listed explicitly in the list.
+(setq stuff-packages
+      '(
+        ;; package stuffs go here
+        evil-paredit
+        glsl-mode
+        mouse-slider-mode
+        ;; palette
+        ;; racket-mode
+        ))
+
+;; List of packages to exclude.
+(setq stuff-excluded-packages '())
+
+;; For each package, define a function stuff/init-<package-stuff>
+;;
+;; (defun stuff/init-my-package ()
+;;   "Initialize my package"
+;;   )
+;;
+;; Often the body of an initialize function uses `use-package'
+;; For more info on `use-package', see readme:
+;; https://github.com/jwiegley/use-package
+
+(defun stuff/init-evil-paredit ()
+  (use-package evil-paredit
+    :defer t
+    :init
+    (progn
+      (add-hook 'emacs-lisp-mode-hook       'evil-paredit-mode)
+      (add-hook 'eval-expression-minibuffer-setup-hook 'evil-paredit-mode)
+      (add-hook 'ielm-mode-hook             'evil-paredit-mode)
+      (add-hook 'lisp-mode-hook             'evil-paredit-mode)
+      (add-hook 'lisp-interaction-mode-hook 'evil-paredit-mode)
+      (add-hook 'scheme-mode-hook           'evil-paredit-mode)
+
+      ;; (add-hook 'slime-repl-mode-hook (lambda () (evil-paredit-mode +1)))
+      )))
+
+(defun stuff/init-glsl-mode ()
+  (use-package glsl-mode
+    :defer t
+    :mode ("\\.\\(vert\\|frag\\|geom\\|glsl\\)\\'" . glsl-mode)))
+
+(defun stuff/init-mouse-slider-mode ()
+  (use-package mouse-slider-mode
+    :defer t
+    :commands mouse-slider-mode
+    :config
+    (progn
+      (add-to-list 'mouse-slider-mode-eval-funcs
+                   '(lisp-mode . slime-compile-defun))
+      ;; (setq mouse-slider-scale 1000)
+      )))
